76bd124faaa648bed3fc499bb34a8c45261d1a5e
kopmoc <55125275+kopmoc@users.noreply.github.com>
2025-05-18 21:22:30 +0800

    Update core.js

--- src/core.js
+++ src/core.js
@@ -270,7 +270,7 @@ export async function handleWebhook(request, ownerUid, botToken, secretToken, ch
     if ("/start" === message.text) {
       // Introduction words for various scenarios
       // 各种情况的介绍词
-      let introduction = "*Welcome\\!* 欢迎\\!*" +
+      let introduction = "*欢迎\\!*" +
           "\n>I'm a PM bot\\. 我是一个私信转发机器人\\。" +
           "\n>I'll forward your messages to my owner, and vice versa\\. 我会把你的消息转发给我的主人，反之亦然\\。" +
           "\n*There are some details below:* *以下是一些详细说明：*" +
@@ -296,6 +296,14 @@ export async function handleWebhook(request, ownerUid, botToken, secretToken, ch
           "  如果你没有看到这些变化，说明编辑内容并未成功转发\\。" +
           "\n>  Perhaps you miss seeing that, you can try edit AGAIN with DIFFERENT CONTENT\\." +
           "  如果你错过了那个动画，你可以尝试再次编辑并更换内容\\。||" +
+  
+          "\n**>DELETE MESSAGE: 删除消息：**" +
+          "\n>  You can delete your messages I forwarded by REPLYING the origin message and TYPING `#del` to me\\." +
+          "  你可以通过回复原始消息并发送 `#del` 来让我删除我转发的消息\\。" +
+          "\n>  No additional process is needed\\. 不需要额外操作\\。" +
+          "\n>  But I can only delete my own messages, not yours\\. 所以我只能删除我发送的消息，无法删除你自己的消息\\。" +
+          "\n>  So, you need to delete the messages for yourself, include \\[origin message\\] \\[command message\\] and \\[notify message\\]\\.\\n" +
+          "  因此，你需要手动删除你自己的消息，包括 \\[原始消息\\]、\\[命令消息\\] 和 \\[通知消息\\]。||" +
   
           "\n" +
           "\n*If you want to see this message again,* *如果你想再次查看此消息，*" +
